4f595cdbb044c7bdd008d6bc260ec7dc8aaa1481
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2023-10-04 21:05:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -48,85 +48,102 @@ function myKeyDown(e)
 {
     keyPressed = e.keyCode;
     console.log(keyPressed);
+    
     if(e.shiftKey == true && keyPressed == '80')
-{
-    console.log("p e shift pressionadas juntas");
-    blockImageWidth = blockImageWidth + 10;
-    blockImageHeight = blockImageHeight + 10;
-    document.getElementById("currentWidth").innerHTML = blockImageWidth;
-    document.getElementById("currentHeight").innerHTML = blockImageHeight;
-}
-if(e.shiftKey && keyPressed == '77')
-{
-    console.log("m e shift pressionadas juntas");
-    blockImageWidth = blockImageWidth - 10;
-    blockImageHeight = blockImageHeight - 10;
-    document.getElementById("currentWidth").innerHTML = blockImageWidth;
-    document.getElementById("currentHeight").innerHTML = blockImageHeight;
-}
-if(keyPressed = '38')
-{
-    up();
-    console.log("cima");
-}
-if(keyPressed = '40')
-{
-    down();
-    console.log("baixo");
-}if(keyPressed = '37')
-{
-    left();
-    console.log("esquerda");
-}if(keyPressed = '39')
-{
-    right();
-    console.log("direita");
-}
-if(keyPressed = '87')
-{
-    newImage('wall.jpg');
-    console.log("w");
-}
-if(keyPressed = '71')
-{
-    newImage('ground.png');
-    console.log("g");
-}
-if(keyPressed = '76')
-{
-    newImage('light_green.png');
-    console.log("l");
-}
-if(keyPressed = '84')
-{
-    newImage('trunk.jpg');
-    console.log("t");
-}
-if(keyPressed = '82')
-{
-    newImage('roof.jpg');
-    console.log("r");
-}
-if(keyPressed = '89')
-{
-    newImage('yellow_wall.png');
-    console.log("y");
-}
-if(keyPressed = '68')
-{
-    newImage('dark_green.png');
-    console.log("d");
-}
-if(keyPressed = '71')
-{
-    newImage('unique.png');
-    console.log("u");
-}
-if(keyPressed = '67')
-{
-    newImage('cloud.jpg');
-    console.log("c");
-}
+    {
+        console.log("p e shift pressionadas juntas");
+        blockImageWidth = blockImageWidth + 10;
+        blockImageHeight = blockImageHeight + 10;
+        document.getElementById("currentWidth").innerHTML = blockImageWidth;
+        document.getElementById("currentHeight").innerHTML = blockImageHeight;
+    }
+    
+    if(e.shiftKey && keyPressed == '77')
+    {
+        console.log("m e shift pressionadas juntas");
+        blockImageWidth = blockImageWidth - 10;
+        blockImageHeight = blockImageHeight - 10;
+        document.getElementById("currentWidth").innerHTML = blockImageWidth;
+        document.getElementById("currentHeight").innerHTML = blockImageHeight;
+    }
+    
+    if(keyPressed == '38')
+    {
+        up();
+        console.log("cima");
+    }
+    
+    if(keyPressed == '40')
+    {
+        down();
+        console.log("baixo");
+    }
+        
+    if(keyPressed == '37')
+    {
+        left();
+        console.log("esquerda");
+    }
+    
+    if(keyPressed == '39')
+    {
+        right();
+        console.log("direita");
+    }
+    
+    if(keyPressed == '87')
+    {
+        newImage('wall.jpg');
+        console.log("w");
+    }
+    
+    if(keyPressed == '71')
+    {
+        newImage('ground.png');
+        console.log("g");
+    }
+
+    if(keyPressed == '76')
+    {
+        newImage('light_green.png');
+        console.log("l");
+    }
+
+    if(keyPressed == '84')
+    {
+        newImage('trunk.jpg');
+        console.log("t");
+    }
+
+    if(keyPressed == '82')
+    {
+        newImage('roof.jpg');
+        console.log("r");
+    }
+
+    if(keyPressed == '89')
+    {
+        newImage('yellow_wall.png');
+        console.log("y");
+    }
+
+    if(keyPressed == '68')
+    {
+        newImage('dark_green.png');
+        console.log("d");
+    }
+
+    if(keyPressed == '71')
+    {
+        newImage('unique.png');
+        console.log("u");
+    }
+
+    if(keyPressed == '67')
+    {
+        newImage('cloud.jpg');
+        console.log("c");
+    }
 }
 
 function up()
